31e2ff6eb2634a8eacc0b96e9cab119df419802a
polyenco <florent.moulon@gmail.com>
2023-01-12 10:22:59 +0100

    Création d'un projet maven

---
+++ projet_tictactoe/src/main/java/apo/laqueuvre_moulon/App.java
@@ -0,0 +1,13 @@
+package apo.laqueuvre_moulon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ projet_tictactoe/src/test/java/apo/laqueuvre_moulon/AppTest.java
@@ -0,0 +1,20 @@
+package apo.laqueuvre_moulon;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
