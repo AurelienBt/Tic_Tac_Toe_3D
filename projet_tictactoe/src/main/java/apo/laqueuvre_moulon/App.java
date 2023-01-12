ebaeb6034c0674ed70e25e1b5935a45c38272981
polyenco <florent.moulon@gmail.com>
2023-01-12 11:12:05 +0100

    ajout d'un diagramme

--- projet_tictactoe/src/main/java/apo/laqueuvre_moulon/App.java
+++ projet_tictactoe/src/main/java/apo/laqueuvre_moulon/App.java
@@ -1,13 +1,7 @@
 package apo.laqueuvre_moulon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
---
+++ projet_tictactoe/to do.md
@@ -0,0 +1,10 @@
+# Gestion de projet
+
+- [x] Créer le git
+- [x] Créer le projet Maven
+- [ ]
+
+# Diagramme UML
+
+- [ ] Faire un diagramme d'activité
+- [ ] Faire un diagramme de class
