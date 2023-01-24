44ef80d425c43ad0f61e48541d60688b8f1ddc30
polyenco <florent.moulon@gmail.com>
2023-01-24 12:50:05 +0100

    Grille2D verif coup

--- projet_tictactoe/src/main/java/apo/boissot_laqueuvre_moulon/Grille2D.java
+++ projet_tictactoe/src/main/java/apo/boissot_laqueuvre_moulon/Grille2D.java
@@ -44,35 +44,74 @@ public class Grille2D extends Grille {
         return false;
     }
 
-    public void placer(String joueur, String numero) {
-        int num = Integer.valueOf(numero) - 1;
+    private int getXCase(String numeroCase) {
+        int numero = Integer.valueOf(numeroCase) - 1;
+        int x = numero % this.taille;
+        return x;
+    }
 
-        int x = num % this.taille;
-        int y = num / this.taille;
+    private int getYCase(String numeroCase) {
+        int numero = Integer.valueOf(numeroCase) - 1;
+        int y = numero / this.taille;
+        return y;
+    }
+
+    public boolean verifieCoup(String input) {
+        int numeroCase = Integer.valueOf(input) - 1;
+        int x = getXCase(input);
+        int y = getYCase(input);
 
         if (x < 0 || x >= this.taille || y < 0 || y >= this.taille) {
-            System.out.println("EREUR ! Le numéro de case " + num + " est invalide");
+            System.out.println("EREUR ! Le numéro de case " + numeroCase + " est invalide");
+            return false;
         } else if (this.grille[y][x].estVide()) {
-            this.grille[y][x].setValeur(joueur);
+            return true;
         } else {
-            System.out.println("EREUR ! La case " + num + " est déjà pleine");
+            System.out.println("EREUR ! La case " + numeroCase + " est déjà pleine");
+            return false;
+        }
+    }
+
+    public void placer(String joueur, String numeroCase) {
+        if (verifieCoup(numeroCase)) {
+            this.grille[getYCase(numeroCase)][getXCase(numeroCase)].setValeur(joueur);
         }
     }
 
     public void testRegretion() {
+        System.out.println("Test Regression pour une grille 3*3");
 
-        // Affichage
+        // afficher
         System.out.println("Une grille vide :");
         afficher();
         System.out.println("");
         System.out.println("");
 
-        // Affichage et placer
+        // verifieCoup
+        System.out.println("Vrai : " + verifieCoup("1"));
+        System.out.println("Vrai : " + verifieCoup("9"));
+        System.out.println("Vrai : " + verifieCoup("8"));
+
         placer("X", "1");
         placer("O", "5");
         placer("X", "9");
+
+        System.out.println("Faux : " + verifieCoup("1"));
+        System.out.println("Faux : " + verifieCoup("5"));
+        System.out.println("Faux : " + verifieCoup("9"));
+        System.out.println("Faux : " + verifieCoup("800"));
+        System.out.println("Faux : " + verifieCoup("0"));
+        System.out.println("Faux : " + verifieCoup("-1"));
+        System.out.println("");
+        System.out.println("");
+
+        // afficher et placer
+        placer("X", "1");
+        placer("O", "5");
+        placer("X", "9");
+        this.grille[0][2].setSelectionnee(true);
         System.out.println("Une grille comme suis :");
-        System.out.println("| X  2  3 |");
+        System.out.println("| X  2 >3<|");
         System.out.println("| 4  O  6 |");
         System.out.println("| 7  8  X |");
         System.out.println("");
@@ -80,17 +119,21 @@ public class Grille2D extends Grille {
         System.out.println("");
         System.out.println("");
 
-        // ViderGrille
+        // viderGrille
         viderGrille();
         System.out.println("Une grille vide :");
         afficher();
         System.out.println("");
         System.out.println("");
 
-    }
+        // tailleEntier
+        System.out.println("1 : " + tailleEntier(5));
+        System.out.println("2 : " + tailleEntier(50));
+        System.out.println("3 : " + tailleEntier(508));
+        System.out.println("3 : " + tailleEntier(100));
+        System.out.println("4 : " + tailleEntier(5555));
+        System.out.println("");
+        System.out.println("");
 
-    public boolean verifieCoup(String input) {
-        return true;
     }
-
 }
