7826ad8505d79b9ecf0602dc3a57b1dfa67791d8
polyenco <florent.moulon@gmail.com>
2023-02-01 11:06:26 +0100

    on arrive vers la fin

--- projet_tictactoe/src/main/java/apo/boissot_laqueuvre_moulon/Joueur.java
+++ projet_tictactoe/src/main/java/apo/boissot_laqueuvre_moulon/Joueur.java
@@ -6,8 +6,10 @@ public abstract class Joueur {
 
     /***
      * Permet à l'utilisateur de choisir son prochain coup
-     * @param text Texte à afficher dans la console 
-     * (en fonction de si le dernier input du joueur n'était pas valide par exemple)
+     * 
+     * @param text    Texte à afficher dans la console
+     *                (en fonction de si le dernier input du joueur n'était pas
+     *                valide par exemple)
      * @param scanner Scanner utilisé pour demander les inputs de l'utilisateur
      * @return String correspondant au coup choisi par le joueur
      */
